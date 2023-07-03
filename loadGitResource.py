28470c6c6b55f87d6c14182a1fb0be59268359c2
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-07-03 14:33:37 +0200

    aider: Refactor load_git_resource and filter_files_to_load functions to take clone_url and repo_path as parameters.
    # Context:
    USER: take url as a parameter, repo path should be temporary

--- loadGitResource.py
+++ loadGitResource.py
@@ -1,19 +1,19 @@
 from langchain.document_loaders import GitLoader
 
-def load_git_resource():
+def load_git_resource(clone_url, repo_path):
     loader = GitLoader(
-        clone_url="https://github.com/hwchase17/langchain",
-        repo_path="./example_data/test_repo2/",
+        clone_url=clone_url,
+        repo_path=repo_path,
         branch="master",
     )
     data = loader.load()
     return len(data)
 
-def filter_files_to_load():
-    loader = GitLoader(repo_path="./example_data/test_repo1/", file_filter=lambda file_path: file_path.endswith(".py"))
+def filter_files_to_load(repo_path):
+    loader = GitLoader(repo_path=repo_path, file_filter=lambda file_path: file_path.endswith(".py"))
 
 # Add any other existing code here
 
 # Add the new function calls here
-load_git_resource()
-filter_files_to_load()
+load_git_resource("https://github.com/hwchase17/langchain", "./example_data/test_repo2/")
+filter_files_to_load("./example_data/test_repo1/")
