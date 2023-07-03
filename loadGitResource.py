7a5e64c639798e0e6b2700f761b9931db7f93b97
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-07-03 14:49:13 +0200

    Refactor load_git_resource to include a branch parameter and update test_load_git_resource to include a file_filter parameter.y

--- loadGitResource.py
+++ loadGitResource.py
@@ -7,7 +7,7 @@ DEFAULT_REPO_PATH = "./example_data/test_repo/"
 DEFAULT_FILE_FILTER = lambda file_path: file_path.endswith(".md")
 
 def load_git_resource(clone_url, repo_path=DEFAULT_REPO_PATH, file_filter=DEFAULT_FILE_FILTER):
-    loader = GitLoader(clone_url=clone_url, repo_path=repo_path, file_filter=file_filter)
+    loader = GitLoader(clone_url=clone_url, repo_path=repo_path, file_filter=file_filter, branch="master")
     data = loader.load()
     shutil.rmtree(repo_path)
     return data
--- test_load_git_resource.py
+++ test_load_git_resource.py
@@ -1,5 +1,5 @@
 from loadGitResource import load_git_resource
 
 clone_url = "https://github.com/hwchase17/langchain"
-data = load_git_resource(clone_url)
+data = load_git_resource(clone_url, file_filter=".py")
 print(data)
