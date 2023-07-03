2dbe23b1443acd52fba7a68113d946666409f78c
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-07-03 14:42:43 +0200

    aider: Create default repo path and file filter.
    # Context:
    USER: create default repo path and file filter

--- loadGitResource.py
+++ loadGitResource.py
@@ -3,7 +3,10 @@ import shutil
 
 __all__ = ['load_git_resource']
 
-def load_git_resource(clone_url, repo_path, file_filter):
+DEFAULT_REPO_PATH = "./example_data/test_repo/"
+DEFAULT_FILE_FILTER = lambda file_path: file_path.endswith(".md")
+
+def load_git_resource(clone_url, repo_path=DEFAULT_REPO_PATH, file_filter=DEFAULT_FILE_FILTER):
     loader = GitLoader(clone_url=clone_url, repo_path=repo_path, file_filter=file_filter)
     data = loader.load()
     shutil.rmtree(repo_path)
