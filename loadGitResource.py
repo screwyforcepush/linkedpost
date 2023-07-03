daa3dcc632a5ca015a9f283faa35eda7ada44d4e
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-07-03 14:42:10 +0200

    aider: Refactored to a single filter loader which loads with filter, loads data, deletes the local files, and returns the data.
    # Context:
    USER: refactor to a single filter loader which loads with filter, loads data, deletes the local files, and returns the data

--- loadGitResource.py
+++ loadGitResource.py
@@ -1,21 +1,10 @@
 from langchain.document_loaders import GitLoader
+import shutil
 
-__all__ = ['load_git_resource', 'filter_files_to_load']
+__all__ = ['load_git_resource']
 
-def load_git_resource(clone_url, repo_path):
-    loader = GitLoader(
-        clone_url=clone_url,
-        repo_path=repo_path,
-        branch="master",
-    )
+def load_git_resource(clone_url, repo_path, file_filter):
+    loader = GitLoader(clone_url=clone_url, repo_path=repo_path, file_filter=file_filter)
     data = loader.load()
-    return len(data)
-
-def filter_files_to_load(repo_path):
-    loader = GitLoader(repo_path=repo_path, file_filter=lambda file_path: file_path.endswith(".md"))
-    loader.load()
-# Add any other existing code here
-
-# Add the new function calls here
-load_git_resource("https://github.com/hwchase17/langchain", "./example_data/test_repo2/")
-filter_files_to_load("./example_data/test_repo1/")
+    shutil.rmtree(repo_path)
+    return data
