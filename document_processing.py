2e24e533c7dd7435029df80b861103a2d4bd9028
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-07-03 15:02:46 +0200

    aider: Refactored `embeddingsFromResource.py` by moving document processing functions to `document_processing.py`.
    # Context:
    USER: refactor `embeddingsFromResource.py` move document processing functions to `document_processing.py`

--- document_processing.py
+++ document_processing.py
@@ -9,3 +9,76 @@ def filter_files_to_load(repo_path, file_filter):
     loader = GitLoader(repo_path=repo_path, file_filter=file_filter)
     data = loader.load()
     return data
+
+def published_to_int(published):
+    return int(published.replace("-", ""))
+
+def process_document_metadata(metadata):
+    pdfLink = False
+    for string in metadata["links"]:
+        if 'pdf' in string:
+            pdfLink = string
+    docMetadata = metadata
+    if "entry_id" in docMetadata:
+        docMetadata["source"] = docMetadata.pop("entry_id")
+    docMetadata["date"] = published_to_int(docMetadata["Published"])
+    docMetadata.pop("Summary")
+    docMetadata = {
+        k: v for k, v in docMetadata.items() if v is not None and v != ""
+    }
+    print(docMetadata)
+
+    return pdfLink, docMetadata
+
+def get_chunks_from_pdf_url(pdfLink, docMetadata):
+    loader = OnlinePDFLoader(pdfLink)
+    data = loader.load()
+    chunks = text_splitter.split_documents(data)
+    for chunk in chunks:
+        chunk.metadata = docMetadata
+    return chunks
+
+def tiktoken_len(text):
+    tokens = tokenizer.encode(text, disallowed_special=())
+    return len(tokens)
+
+def get_docs(query):
+    MAX_RETRIES = 100
+    for i in range(MAX_RETRIES):
+        try:
+            docs = retriever.get_relevant_documents(query=query)
+            return docs
+            break
+        except ConnectionResetError:
+            if i < MAX_RETRIES - 1:  # i is zero indexed
+                print("doc connection reset... retrying")
+                time.sleep(1)  # wait a bit before trying again
+                continue
+            else:
+                raise
+
+def get_doc_queries(filename):
+    # If file doesn't exist, document hasn't been added yet
+    if not os.path.exists(filename):
+        return False
+
+    with open(filename, 'r') as f:
+        docqueries = json.load(f)
+
+    # Check if the title is in the file
+    return docqueries
+
+def remove_doc_query(filename, query):
+    titles = []
+
+    # If file exists, load existing data
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            titles = json.load(f)
+
+    # Add the new title
+    titles.remove(query)
+
+    # Write the data back to the file
+    with open(filename, 'w') as f:
+        json.dump(titles, f, indent=4)
--- embeddingsFromResource.py
+++ embeddingsFromResource.py
@@ -58,58 +58,7 @@ def is_content_related_to_topic(content, topic):
     response = chain.run(content=content, topic=topic)
     return response in ("True", "true", "True.", "true.")
 
-
-# %%
-def get_docs(query):
-    MAX_RETRIES = 100
-    for i in range(MAX_RETRIES):
-        try:
-            docs = retriever.get_relevant_documents(query=query)
-            return docs
-            break
-        except ConnectionResetError:
-            if i < MAX_RETRIES - 1:  # i is zero indexed
-                print("doc connection reset... retrying")
-                time.sleep(1)  # wait a bit before trying again
-                continue
-            else:
-                raise
-
-
-# %%
-
-def published_to_int(published):
-    return int(published.replace("-", ""))
-
-def process_document_metadata(metadata):
-    pdfLink = False
-    for string in metadata["links"]:
-        if 'pdf' in string:
-            pdfLink = string
-    docMetadata = metadata
-    if "entry_id" in docMetadata:
-        docMetadata["source"] = docMetadata.pop("entry_id")
-    docMetadata["date"] = published_to_int(docMetadata["Published"])
-    docMetadata.pop("Summary")
-    docMetadata = {
-        k: v for k, v in docMetadata.items() if v is not None and v != ""
-    }
-    print(docMetadata)
-
-    return pdfLink, docMetadata
-
-
-# %%
-
-
 tokenizer = tiktoken.get_encoding("cl100k_base")
-
-
-# create the length function
-def tiktoken_len(text):
-    tokens = tokenizer.encode(text, disallowed_special=())
-    return len(tokens)
-
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
@@ -118,50 +67,6 @@ text_splitter = RecursiveCharacterTextSplitter(
     separators=["\n\n", "\n", " ", ""],
 )
 
-
-def get_chunks_from_pdf_url(pdfLink, docMetadata):
-    loader = OnlinePDFLoader(pdfLink)
-    data = loader.load()
-    chunks = text_splitter.split_documents(data)
-    for chunk in chunks:
-        chunk.metadata = docMetadata
-    return chunks
-
-
-# %%
-embeddings = OpenAIEmbeddings()
-
-texts = ["this is the first chunk of text", "then another second chunk of text is here"]
-
-res = embeddings.embed_documents(texts)
-len(res), len(res[0])
-
-
-# %%
-
-pinecone.init(
-    api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENV")
-)
-index_name = "langchain-demo"
-if index_name not in pinecone.list_indexes():
-    # we create a new index
-    pinecone.create_index(
-        name=index_name,
-        metric="cosine",
-        dimension=len(res[0]),  # 1536 dim of text-embedding-ada-002
-    )
-
-
-# %%
-
-index = pinecone.GRPCIndex(index_name)
-# wait a moment for the index to be fully initialized
-time.sleep(1)
-
-index.describe_index_stats()
-
-
-# %%
 def upsert_chunks(chunks, namespace):
     docsearch = Pinecone.from_documents(
         chunks, embeddings, index_name=index_name, namespace=namespace
@@ -185,7 +90,6 @@ def check_if_exists(metadata, filename):
 
     return False
 
-
 def add_metadata(metadata, filename):
     data = []
 
@@ -201,38 +105,12 @@ def add_metadata(metadata, filename):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
 
-
-def get_doc_queries(filename):
-    # If file doesn't exist, document hasn't been added yet
-    if not os.path.exists(filename):
-        return False
-
-    with open(filename, 'r') as f:
-        docqueries = json.load(f)
-
-    # Check if the title is in the file
-    return docqueries
-def remove_doc_query(filename, query):
-    titles = []
-
-    # If file exists, load existing data
-    if os.path.exists(filename):
-        with open(filename, 'r') as f:
-            titles = json.load(f)
-
-    # Add the new title
-    titles.remove(query)
-
-    # Write the data back to the file
-    with open(filename, 'w') as f:
-        json.dump(titles, f, indent=4)
-# %%
 namespace = "ai-research"
 newerthan = 20230000
 filename = 'metadata.json'
 doc_queries_filename = 'titles.json'
 doc_queries = get_doc_queries(doc_queries_filename)
-# %%
+
 for query in doc_queries:
     print("getting research for ", query)
     docs = get_docs(query="generative ai prompt engineering")
@@ -252,5 +130,3 @@ for query in doc_queries:
             docsearch = upsert_chunks(chunks, namespace)
             add_metadata(doc.metadata, filename)
     remove_doc_query(doc_queries_filename, query)
-
-# %%
