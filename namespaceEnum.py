d5abd128af92781144e685705e24857fca78f1bf
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-07-07 15:37:59 +0200

    aider: Added a `get_all_namespaces` function to `PineconeNamespaceEnum` class in `namespaceEnum.py` that returns a dictionary of all enum members with their descriptions.
    # Context:
    USER: create a funciton in `namespaceEnum.py` that returns all PineconeNamespaceEnums with discriptions
    ASSISTANT: To create a function that returns all `PineconeNamespaceEnum` with descriptions, we need to modify the `namespaceEnum.py` file.
    
    Here's how we can do it:
    
    1. Add a new function `get_all_namespaces` in the `PineconeNamespaceEnum` class. This function will iterate over all the members of the `PineconeNamespaceEnum` and return a dictionary where the keys are the names of the enum members and the values are their descriptions.
    
    Here is the *edit block* for this change:
    
    ```python
    namespaceEnum.py
    <<<<<<< ORIGINAL
    from enum import Enum
    
    class PineconeNamespaceEnum(str, Enum):
        AI_RESEARCH = 'ai-research'
        VIDEO_STREAMING_ANALYTICS = 'video-streaming-analytics'
        AI_ENGINEERING_DOCUMENTATION = 'ai-engineering-documentation'
    =======
    from enum import Enum
    
    class PineconeNamespaceEnum(str, Enum):
        AI_RESEARCH = 'ai-research'
        VIDEO_STREAMING_ANALYTICS = 'video-streaming-analytics'
        AI_ENGINEERING_DOCUMENTATION = 'ai-engineering-documentation'
    
        @classmethod
        def get_all_namespaces(cls):
            return {member.name: member.value for member in cls}
    >>>>>>> UPDATED
    ```
    
    Please note that the descriptions of the enum members are their values. If you want to add more detailed descriptions, you might need to modify the structure of the `PineconeNamespaceEnum` class.

--- namespaceEnum.py
+++ namespaceEnum.py
@@ -4,3 +4,7 @@ class PineconeNamespaceEnum(str, Enum):
     AI_RESEARCH = 'ai-research'
     VIDEO_STREAMING_ANALYTICS = 'video-streaming-analytics'
     AI_ENGINEERING_DOCUMENTATION = 'ai-engineering-documentation'
+
+    @classmethod
+    def get_all_namespaces(cls):
+        return {member.name: member.value for member in cls}
