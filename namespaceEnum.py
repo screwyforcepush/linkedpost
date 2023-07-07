da6bd1b996c1b88123eeaf8c1419d244403c9007
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-07-07 15:43:32 +0200

    aider: Change enum members in `PineconeNamespaceEnum` class to include descriptions and modify related methods to return descriptions instead of values.
    # Context:
    USER:
    the values are
    ai-research: catalog of ai academic research papers
    video-streaming-analytics: best practices, metrics, and analytics within the video streaming domain
    ai-engineering-documentation: code documentation and readme files for ai engineering libraries
    
    ASSISTANT: To add descriptions to the enum members, we need to modify the structure of the `PineconeNamespaceEnum` class in the `namespaceEnum.py` file.
    
    Here's how we can do it:
    
    1. Change the enum members to be tuples where the first element is the value and the second element is the description.
    2. Add a property `description` to the `PineconeNamespaceEnum` class that returns the description of the enum member.
    3. Modify the `get_all_namespaces` function to return a dictionary where the keys are the names of the enum members and the values are their descriptions.
    
    Here is the *edit block* for this change:
    
    ```python
    namespaceEnum.py
    <<<<<<< ORIGINAL
    from enum import Enum
    
    class PineconeNamespaceEnum(str, Enum):
        AI_RESEARCH = 'ai-research'
        VIDEO_STREAMING_ANALYTICS = 'video-streaming-analytics'
        AI_ENGINEERING_DOCUMENTATION = 'ai-engineering-documentation'
    
        @classmethod
        def get_all_namespaces(cls):
            return {member.name: member.value for member in cls}
    =======
    from enum import Enum
    
    class PineconeNamespaceEnum(Enum):
        AI_RESEARCH = ('ai-research', 'catalog of ai academic research papers')
        VIDEO_STREAMING_ANALYTICS = ('video-streaming-analytics', 'best practices, metrics, and analytics within the video streaming domain')
        AI_ENGINEERING_DOCUMENTATION = ('ai-engineering-documentation', 'code documentation and readme files for ai engineering libraries')
    
        @property
        def value(self):
            return self.value[0]
    
        @property
        def description(self):
            return self.value[1]
    
        @classmethod
        def get_all_namespaces(cls):
            return {member.name: member.description for member in cls}
    >>>>>>> UPDATED
    ```
    
    Please note that the `value` property now returns the first element of the tuple and the `description` property returns the second element of the tuple. The `get_all_namespaces` function now returns the descriptions of the enum members.

--- namespaceEnum.py
+++ namespaceEnum.py
@@ -1,10 +1,18 @@
 from enum import Enum
 
-class PineconeNamespaceEnum(str, Enum):
-    AI_RESEARCH = 'ai-research'
-    VIDEO_STREAMING_ANALYTICS = 'video-streaming-analytics'
-    AI_ENGINEERING_DOCUMENTATION = 'ai-engineering-documentation'
+class PineconeNamespaceEnum(Enum):
+    AI_RESEARCH = ('ai-research', 'catalog of ai academic research papers')
+    VIDEO_STREAMING_ANALYTICS = ('video-streaming-analytics', 'best practices, metrics, and analytics within the video streaming domain')
+    AI_ENGINEERING_DOCUMENTATION = ('ai-engineering-documentation', 'code documentation and readme files for ai engineering libraries')
+
+    @property
+    def value(self):
+        return self.value[0]
+
+    @property
+    def description(self):
+        return self.value[1]
 
     @classmethod
     def get_all_namespaces(cls):
-        return {member.name: member.value for member in cls}
+        return {member.name: member.description for member in cls}
