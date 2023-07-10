7eb2b7d80d636544eb731cbc238e08661a79d001
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-07-10 20:11:32 +0200

    aider: Added a new class `PineconeNamespaceDescription` and a function `get_all_namespaces` to provide descriptions for each namespace in `PineconeNamespaceEnum`.
    # Context:
    USER: provide these features seperatly
    ASSISTANT: To provide the `description` property and the `get_all_namespaces` method separately, we can create a new class `PineconeNamespaceDescription` that maps the namespace names to their descriptions. We can also create a standalone function `get_all_namespaces` that uses this new class.
    
    Here are the changes:
    
    ```python
    namespaceEnum.py
    <<<<<<< ORIGINAL
    from enum import Enum
    
    class PineconeNamespaceEnum(str, Enum):
        AI_RESEARCH = 'ai-research'
        VIDEO_STREAMING_ANALYTICS = 'video-streaming-analytics'
        AI_ENGINEERING_DOCUMENTATION = 'ai-engineering-documentation'
    =======
    from enum import Enum
    
    class PineconeNamespaceEnum(str, Enum):
        AI_RESEARCH = 'ai-research'
        VIDEO_STREAMING_ANALYTICS = 'video-streaming-analytics'
        AI_ENGINEERING_DOCUMENTATION = 'ai-engineering-documentation'
    
    class PineconeNamespaceDescription:
        AI_RESEARCH = 'catalog of ai academic research papers'
        VIDEO_STREAMING_ANALYTICS = 'best practices, metrics, and analytics within the video streaming domain'
        AI_ENGINEERING_DOCUMENTATION = 'code documentation and readme files for ai engineering libraries'
    
    def get_all_namespaces():
        return {member: PineconeNamespaceDescription.__dict__[member] for member in PineconeNamespaceEnum.__members__}
    >>>>>>> UPDATED
    ```
    
    In the updated code, `PineconeNamespaceDescription` is a simple class that maps the namespace names to their descriptions. The `get_all_namespaces` function returns a dictionary that maps the namespace names to their descriptions. It uses the `__members__` attribute of `PineconeNamespaceEnum` to get the names of all namespaces and the `__dict__` attribute of `PineconeNamespaceDescription` to get the descriptions.

--- namespaceEnum.py
+++ namespaceEnum.py
@@ -4,3 +4,11 @@ class PineconeNamespaceEnum(str, Enum):
     AI_RESEARCH = 'ai-research'
     VIDEO_STREAMING_ANALYTICS = 'video-streaming-analytics'
     AI_ENGINEERING_DOCUMENTATION = 'ai-engineering-documentation'
+
+class PineconeNamespaceDescription:
+    AI_RESEARCH = 'catalog of ai academic research papers'
+    VIDEO_STREAMING_ANALYTICS = 'best practices, metrics, and analytics within the video streaming domain'
+    AI_ENGINEERING_DOCUMENTATION = 'code documentation and readme files for ai engineering libraries'
+
+def get_all_namespaces():
+    return {member: PineconeNamespaceDescription.__dict__[member] for member in PineconeNamespaceEnum.__members__}
