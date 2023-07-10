864aa8385b3deb8d6a8e23b31a29c0406b0ca2a8
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-07-10 20:10:38 +0200

    aider: Replaced PineconeNamespaceEnum class with a new one that inherits from str and Enum, removing the description property and get_all_namespaces method.
    # Context:
    USER: create PineconeNamespaceEnum(str, Enum) that contains the member.values of the existing PineconeNamespaceEnum(enum)
    ASSISTANT: To create a new `PineconeNamespaceEnum` that inherits from `str` and `Enum`, we need to modify the `namespaceEnum.py` file. We will replace the existing `PineconeNamespaceEnum` class with a new one that inherits from `str` and `Enum`. The new class will have the same member values as the existing one.
    
    Here is the change:
    
    ```python
    namespaceEnum.py
    <<<<<<< ORIGINAL
    from enum import Enum
    
    class PineconeNamespaceEnum(Enum):
        AI_RESEARCH = ('ai-research', 'catalog of ai academic research papers')
        VIDEO_STREAMING_ANALYTICS = ('video-streaming-analytics', 'best practices, metrics, and analytics within the video streaming domain')
        AI_ENGINEERING_DOCUMENTATION = ('ai-engineering-documentation', 'code documentation and readme files for ai engineering libraries')
    
        @property
        def value(self):
            return self._value_[0]
    
        @property
        def description(self):
            return self._value_[1]
    
        @classmethod
        def get_all_namespaces(cls):
            return {member.value: member.description for member in cls}
    
        @classmethod
        def get_all_namespace_names(cls):
            return [member.value for member in cls]
    =======
    from enum import Enum
    
    class PineconeNamespaceEnum(str, Enum):
        AI_RESEARCH = 'ai-research'
        VIDEO_STREAMING_ANALYTICS = 'video-streaming-analytics'
        AI_ENGINEERING_DOCUMENTATION = 'ai-engineering-documentation'
    >>>>>>> UPDATED
    ```
    
    Please note that the new `PineconeNamespaceEnum` class does not have the `description` property and the `get_all_namespaces` method because it's not clear how to handle descriptions in the new class. If you need these features, please provide more details.

--- namespaceEnum.py
+++ namespaceEnum.py
@@ -1,22 +1,6 @@
 from enum import Enum
 
-class PineconeNamespaceEnum(Enum):
-    AI_RESEARCH = ('ai-research', 'catalog of ai academic research papers')
-    VIDEO_STREAMING_ANALYTICS = ('video-streaming-analytics', 'best practices, metrics, and analytics within the video streaming domain')
-    AI_ENGINEERING_DOCUMENTATION = ('ai-engineering-documentation', 'code documentation and readme files for ai engineering libraries')
-
-    @property
-    def value(self):
-        return self._value_[0]
-
-    @property
-    def description(self):
-        return self._value_[1]
-
-    @classmethod
-    def get_all_namespaces(cls):
-        return {member.value: member.description for member in cls}
-    
-    @classmethod
-    def get_all_namespace_names(cls):
-        return [member.value for member in cls]
+class PineconeNamespaceEnum(str, Enum):
+    AI_RESEARCH = 'ai-research'
+    VIDEO_STREAMING_ANALYTICS = 'video-streaming-analytics'
+    AI_ENGINEERING_DOCUMENTATION = 'ai-engineering-documentation'
