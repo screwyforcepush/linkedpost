2847e9a45b2224098aa8a6bf5c76df402511bdef
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-07-11 14:41:17 +0200

    aider: Refactored `get_content_from_db.py` to ensure that the `pinecone.init` line is only executed when the `get_content_from_db` function is called, not when the file is imported.
    # Context:
    USER: ensure `get_content_from_db.py` function can be called from other files. pinecone.init line is a dependency
    ASSISTANT: To make the function `get_content_from_db` callable from other files, we need to ensure that the function is defined in the global scope of the `get_content_from_db.py` file. From the provided code, it seems like it is already in the global scope.
    
    However, the `pinecone.init` line is a dependency that is executed when the file is imported. To avoid this, we can wrap the initialization code in a function and call this function inside `get_content_from_db`.
    
    Here is how you can do it:
    
    ```python
    get_content_from_db.py
    <<<<<<< ORIGINAL
    pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENV"))
    index_name = "langchain-demo"
    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
    
    def get_content_from_db(namespace, db_query, k=4):
        db = Pinecone.from_existing_index(index_name, embeddings, namespace=namespace)
        docs = db.similarity_search(db_query,k=k)
        docs_page_content = " ".join([d.page_content for d in docs])
        # print(docs_page_content)
        return docs_page_content
    =======
    def init_pinecone():
        pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENV"))
        global index_name
        index_name = "langchain-demo"
        global embeddings
        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
    
    def get_content_from_db(namespace, db_query, k=4):
        init_pinecone()
        db = Pinecone.from_existing_index(index_name, embeddings, namespace=namespace)
        docs = db.similarity_search(db_query,k=k)
        docs_page_content = " ".join([d.page_content for d in docs])
        # print(docs_page_content)
        return docs_page_content
    >>>>>>> UPDATED
    ```
    
    This way, the `pinecone.init` line will only be executed when `get_content_from_db` is called, not when the file is imported.

--- get_content_from_db.py
+++ get_content_from_db.py
@@ -14,11 +14,15 @@ openai = OpenAI(
     openai_api_key=OPENAI_API_KEY
 )
 
-pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENV"))
-index_name = "langchain-demo"
-embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+def init_pinecone():
+    pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENV"))
+    global index_name
+    index_name = "langchain-demo"
+    global embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
 def get_content_from_db(namespace, db_query, k=4):
+    init_pinecone()
     db = Pinecone.from_existing_index(index_name, embeddings, namespace=namespace)
     docs = db.similarity_search(db_query,k=k)
     docs_page_content = " ".join([d.page_content for d in docs])
