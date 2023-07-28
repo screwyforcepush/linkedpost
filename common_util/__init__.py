af62b41bb331b1744fb0cb70823684a7f255f459
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-07-28 14:21:28 +0200

    Remove unnecessary comment in __init__.py.

--- common_util/__init__.py
+++ common_util/__init__.py
@@ -2,4 +2,3 @@ import os
 import glob
 
 __all__ = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(os.path.abspath(__file__)) + "/*.py")]
-# This is an empty file that marks the directory as a Python package
