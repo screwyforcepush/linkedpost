b1446b2acaae9e6075101676a3daa5f1e02bd3e8
Alex Savage <savageabscbn@ABSMacBook-Pro.local>
2023-08-01 15:05:12 +0200

    aider: Added timestamp to the `set_seed_query` argument string.
    # Context:
    USER: add timestamp to the set_seed_query argument string
    ASSISTANT: To add a timestamp to the `set_seed_query` argument string, we need to modify the `conductor.py` file.
    
    Here are the steps:
    1. Import the `datetime` module.
    2. In the function where `set_seed_query` is called, create a timestamp string using the current date and time.
    3. Append the timestamp string to the `set_seed_query` argument.
    
    Here is the code change:
    
    ```python
    conductor.py
    <<<<<<< ORIGINAL
        from common_util.memory import set_entity_memory_long_cache, warm_cache, get_entities
        from queryDB import tools, set_seed_query, get_seed_query, get_latest_ai_research
        from langchain_experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
        import langchain
    
        langchain.debug = True
    
        set_seed_query("Find an interesting and unique connection between different Generative AI research findings and techniques. Include all information that would aid in the application of the connected techniques to Video Streaming Analytics.")
        set_entity_memory_long_cache([])
        latest_research = get_latest_ai_research()
        entities = get_entities()
    =======
        from common_util.memory import set_entity_memory_long_cache, warm_cache, get_entities
        from queryDB import tools, set_seed_query, get_seed_query, get_latest_ai_research
        from langchain_experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
        import langchain
        from datetime import datetime
    
        langchain.debug = True
    
        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
        set_seed_query("Find an interesting and unique connection between different Generative AI research findings and techniques. Include all information that would aid in the application of the connected techniques to Video Streaming Analytics. " + timestamp)
        set_entity_memory_long_cache([])
        latest_research = get_latest_ai_research()
        entities = get_entities()
    >>>>>>> UPDATED
    ```
    
    Please replace `"Find an interesting and unique connection between different Generative AI research findings and techniques. Include all information that would aid in the application of the connected techniques to Video Streaming Analytics."` with the actual string where `set_seed_query` is called.

--- conductor.py
+++ conductor.py
@@ -1,13 +1,14 @@
-from common_util.memory import set_entity_memory_long_cache, warm_cache, get_entities
-from queryDB import tools, set_seed_query, get_seed_query, get_latest_ai_research
-from langchain_experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
-import langchain
-from common_util.llms import LLM_CHAT, LLM_FACT, LLM_PLAN
+    from common_util.memory import set_entity_memory_long_cache, warm_cache, get_entities
+    from queryDB import tools, set_seed_query, get_seed_query, get_latest_ai_research
+    from langchain_experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
+    import langchain
+    from datetime import datetime
 
-# %%
-langchain.debug = True
+    langchain.debug = True
 
-set_seed_query("Find an interesting and unique connection between different Generative AI research findings and techniques. Include all information that would aid in the application of the connected techniques to Video Streaming Analytics.")
-set_entity_memory_long_cache([])
-latest_research = get_latest_ai_research()
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    set_seed_query("Find an interesting and unique connection between different Generative AI research findings and techniques. Include all information that would aid in the application of the connected techniques to Video Streaming Analytics. " + timestamp)
+    set_entity_memory_long_cache([])
+    latest_research = get_latest_ai_research()
+    entities = get_entities()
 entities = get_entities()
